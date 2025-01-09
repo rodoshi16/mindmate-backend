cc19e3f3d6100a43a4f72b5f0ede304526675b5b
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2025-01-08 23:16:27 -0500

    Leetcode 234 solution

--- PycharmProjects/pythonProject66/main.py
+++ PycharmProjects/pythonProject66/main.py
@@ -24,20 +24,45 @@ def lengthOfLIS(nums) -> int:
     return len(long_seq)
 
 
+def transpose(matrix):
+
+    total = len(matrix[0]) + len(matrix)
+    col_diff = total - len(matrix[0])
+    row_diff = total - len(matrix)
+
+    for i in range(len(matrix)):
+        for k in range(col_diff):
+            matrix[i].append(0)
+
+        for j in range(len(matrix)):
+            for m in range(row_diff):
+                matrix[j].append(0)
+
+    return matrix
+
+# Leetcode 997 using Two pointer approach
+def sortedSquares(nums: list[int]) -> list[int]:
+    left = 0
+    right = len(nums) - 1
+    result = []
+
+    while left <= right:
+        if abs(nums[left]) > abs(nums[right]):
+            result.append(nums[left] ** 2)
+            left += 1
+        else:
+            result.append(nums[right] ** 2)
+            right -= 1
+
+    result.reverse()
+    return result
 
 
-
-
-
-
-
-
-
-
-    # flag = True
-    # longest_seq = []
-    # for i in range(len(nums) - 1):
-    #     if nums[i + 1] < nums[i]:
-    #         flag = False
-    #     longest_seq.append(nums[i])
-    # return len(longest_seq)
+#234 Palindrome Linked List
+def isPalindrome(head: [ListNode]) -> bool:
+    curr = head
+    lst = []
+    while curr is not None:
+        lst.append(curr.val)
+        curr = curr.next
+    return lst[::-1] == lst
