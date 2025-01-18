6217ae3f7f5f7a0c0f5dcb01c416081fda42793b
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2025-01-17 23:36:08 -0500

    Leetcode 1768

--- PycharmProjects/pythonProject66/DSA/Arrays and Strings/arrays.py
+++ PycharmProjects/pythonProject66/DSA/Arrays and Strings/arrays.py
@@ -20,3 +20,26 @@ def findClosestNumber(nums: list[int]) -> int:
                     distance = abs(item)
                     largest_value = item
     return largest_value
+
+#Leetcode 1768
+class Solution:
+    def mergeAlternately(word1: str, word2: str) -> str:
+        result = ''
+        max_lst= []
+        min_lst = []
+        max_len = max(len(word1), len(word2))
+        for i in range(min(len(word1), len(word2))):
+            result += word1[i]
+            result += word2[i]
+
+        if len(word1) == max_len:
+            max_lst = word1
+            min_lst = word2
+        else:
+            max_lst = word2
+            min_lst = word1
+
+        result += max_lst[len(min_lst):]
+        return result
+
+
